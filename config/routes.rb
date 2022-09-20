552957bbe115a48a5d4e0f04592ac5bc4bb56501
wuw-github <wuw1998@gmail.com>
2022-09-19 23:05:27 -0400

    before CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root "home#index"
 
-  get 'about' => "pages#about"
-
-  resources :posts
 end
